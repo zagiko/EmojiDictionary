1496950f1eec6db147813d1db4ad3ade4c97f43c
zagiko <m.sagik@gmail.com>
2023-04-13 13:55:20 +0300

    add file

---
+++ EmojiDictionary/EmojiDictionary/EmojiDictionaryCollectionViewCellColumnItem.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiDictionaryCollectionViewCellColumnItem.swift
+//  EmojiDictionary
+//
+//  Created by Mykhailo Zagiko on 13.04.2023.
+//
+
+import UIKit
+
+class EmojiDictionaryCollectionViewCellColumnItem: UICollectionViewCell {
+    
+}
